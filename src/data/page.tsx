56dc4548470cd851f340fd70aba97d5c7deb85e2
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-04-14 10:43:45 +0500

    update aboutMe in data

--- src/data/page.tsx
+++ src/data/page.tsx
@@ -15,8 +15,8 @@ export const personalData = {
 export const aboutMe = {
   title: "About Me",
   body: [
-    "Fueled by a passion for creating a realistic VR space travel simulator with the generative AI's 'super magical power,' I'm an eager learner currently engrossed in the world of Computer Science. My fascination with technology and science fiction has always driven my desire to understand how things work and create something truly innovative. The Cloud Applied Generative AI Engineer (GenEng) program is the perfect launchpad to bring my dream VR space adventure to life!",
-    "In my free time, I love getting lost in the world of technology and science fiction. Their endless possibilities fuel my creativity and drive to learn. I'm excited to connect with other generative AI enthusiasts and together, push the boundaries of what's possible!",
+    "Fueled by a passion for leveraging the super magical Generative AI technology to create a realistic VR space travel simulator, I'm an eager learner currently immersed in the world of Computer Science. My fascination with technology and science fiction has always driven me to understand how things work and create something truly innovative. The Cloud Applied Generative AI Engineer (GenEng) program is the perfect launchpad to bring my dream VR space adventure to life!",
+    "In my free time, I love immersing myself in the world of technology and science fiction. Their endless possibilities fuel my creativity and drive to learn. I'm excited to connect with other Generative AI enthusiasts and, together, push the boundaries of what's possible!",
   ],
 };
 
