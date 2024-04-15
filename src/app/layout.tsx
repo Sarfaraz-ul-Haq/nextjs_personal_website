796b1ecd209a2917b69880246ed93174c6ac20d5
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-04-15 18:30:51 +0500

    put children inside main tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,12 +18,13 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  console.log(personalData);
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="{inter.className} flex flex-col sm:flex-row ">
         <Sidebar data={personalData} />
-        {children}
+        <main className="grow-full p-8 sm:p-16 w-full sm:basis-2/3  ml-auto">
+          {children}
+        </main>
       </body>
     </html>
   );
