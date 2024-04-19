cd8fd8b02d1ea641153e6d38303ef173c0944442
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-04-19 21:56:58 +0500

    use components in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import AboutMe from "@/components/about_me";
+import { aboutMe, skills } from "@/data/page";
+import ProfessionalExperience from "@/components/professional_experience";
+import { professionalData } from "@/data/page";
+
 export default function Home() {
-  return <div>Hello, this is my personal website</div>;
+  return (
+    <div>
+      <AboutMe data={aboutMe} skills={skills} />
+      <ProfessionalExperience data={professionalData} />
+    </div>
+  );
 }
